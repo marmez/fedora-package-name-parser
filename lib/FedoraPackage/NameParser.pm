a5c44bc64d197659a82fa9205371bbb8292d6e4a
Martin Mezl <mezlmartin1@gmail.com>
2018-04-28 12:51:15 +0200

    POD documents the future changes

--- lib/FedoraPackage/NameParser.pm
+++ lib/FedoraPackage/NameParser.pm
@@ -11,18 +11,6 @@ sub new {
 
 sub parse {
   my ($self, $string) = @_;
-  # $string contain:
-  # * package base name string (any character to first period)
-  # * period (not needed)
-  # * package architecture string (x86_64, i686, noarch)
-  # * one space (not needed)
-  # * package full version string (including number but without milestone number)
-  # * dash (not needed)
-  # * package milestone number - release
-  # * optionally: period (not needed)
-  # * optionally: package distribution version string (fc = Fedora Core + number)
-  # * optionally: period (not needed)
-  # * optionally: package repository mark
   my ($name_arch, $fullversion_milestone_other) = split / /, $string;
   my ($__1, $__2, $__3, $__4, $__5, $__6);
   my $milestone_other;
@@ -119,4 +107,140 @@ sub get_restofstr {
 }
 
 1;
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+C<FedoraPackage::NameParser> - Parse a Fedora RPM package name to its components.
+
+=head1 SYNOPSIS
+
+  use FedoraPackage::NameParser;
+
+  my $fpnp = FedoraPackage::NameParser->new;
+
+  # Parse a string containing single package name
+  $fpnp->parse($package_name)
+    or die 'Could not parse package name';
+
+  # Parse a string containing more package names
+  while ($string) {
+    $fpnp->parse($string)
+      or die 'Could not parse string for package name';
+    $string = $fpnp->get('rest');
+  }
+
+  # Get the package name components
+  # Package name
+  $fpnp->get('name');
+  # Package architecture
+  $fpnp->get('arch');
+  # Package number (optional)
+  $fpnp->get('num');
+  # Package version number
+  $fpnp->get('ver');
+  # Package milestone number
+  $fpnp->get('milst');
+  # Package distribution tag
+  $fpnp->get('dist');
+  # Package resository tag
+  $fpnp->get('repo');
+
+  # Get rest of string
+  $fpnp->get('rest');
+
+  # Get full package name (composed)
+  $fpnp->fullname;
+
+=head1 DESCRIPTION
+
+Parse the Fedora RPM package name and get its parts separately.
+
+=head1 METHODS
+
+=head2 C<new>
+
+  my $fpnp = FedoraPackage::NameParser->new;
+
+Create a new FedoraPackage::NameParser object.
+
+=head2 C<parse>
+
+  $fpnp->parse($string);
+
+Parse the first package name in the C<$string> and store the results internally. Package name components can be returned by the C<-E<gt>get($component_name)> method. If the C<$string> contains more than one package name, the first name is removed and rest of the C<$string> is stored internally and can be found by a C<-E<gt>get('rest')> method call. Returns C<1> if the package name was successfully parsed, C<0> otherwise.
+
+=head2 C<get>
+
+  $fpnp->get($component_name);
+  $fpnp->get('rest');
+
+Get single component of the package name or the rest of string (without the first package name already parsed).
+
+Available component names (C<$component_name>):
+
+=over
+
+=item *
+
+C<name> - Package name. Package base name string (any character to first period).
+
+=item *
+
+C<arch> - Package architecture (one of 'x86_64', 'i686', 'noarch').
+
+=item *
+
+C<num> - Package number (optional).
+
+=item *
+
+C<ver> - Package version number.
+
+=item *
+
+C<milst> - Package milestone number (release).
+
+=item *
+
+C<dist> - Package distribution tag (optional, 'fc' = Fedora Core + number).
+
+=item *
+
+C<repo> - Package resository tag (optional).
+
+=item *
+
+C<rest> - Rest of string (if the string contains more package names).
+
+=back
+
+=head2 C<fullname>
+
+  $fpnp->fullname;
+
+Get full package name (composed again).
+
+=head1 NOTES
+
+=head2 Names of Fedora RPM Packages
+
+=head3 Links to Source Fedora Project Wiki Pages
+
+=over
+
+=item *
+
+L<Guidelines for Naming Fedora Packages - Fedora Project Wiki|https://fedoraproject.org/wiki/Packaging:Naming>
+
+=item *
+
+L<Guidelines for Versioning Fedora Packages - Fedora Project Wiki|https://fedoraproject.org/wiki/Packaging:Versioning>
+
+=back
+
+=cut
 
